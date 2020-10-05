9b749846fc62c7a1276efb94e03c509445728c34
AlexanderBelfort <alexanderphoenixbusiness@mail.bg>
2020-10-05 13:47:54 +0100

    fin

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -4,6 +4,8 @@ import cv2
 #Tells us if the image contains a face or not
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
 #the face detector is based on the VIOLA-JONES Algorithm
 #error might pop up saying cv2 has no cascadeclassifier member
 #so we go to settings.json and add "python.linting.pylintArgs": ["--generate-members"]
@@ -30,6 +32,7 @@ while True:
     #multiscale = multiple sizes of faces
     faces = face_detector.detectMultiScale(frame_grayscale)
 
+
     #run smile detection within each of those faces
     for (x, y, w, h) in faces:
 
@@ -38,6 +41,33 @@ while True:
         #we only needed the frame_grayscale to get the caluclations
         #so we use the colored image
         cv2.rectangle(frame, (x, y),(x+w, y+h), (100, 200, 50), 4)
+
+        #get the subframe using numpy N-dimensional array slicing
+        the_face = frame[y:y+h, x:x+w]
+
+        #change to grayscale
+        face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
+
+        #to optimize finding the smile, we will use
+        #scaleFactor=1.7 which will essentially blur the picture
+        #and look for a Smile in a blurred enough image
+        smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
+
+        #find all SMILES in THE FACE
+        for (x_, y_, w_, h_) in smiles:
+            
+            #look for smiles only IN FACES cause
+            #random smiles dont pop out in the wild out of nowhere
+            #maybe just on billboards/dental ads
+            #draw a rectangle around them
+            cv2.rectangle(the_face, (x_, y_),(x_ + w_, y_ + h_), (50, 50, 200), 4)
+
+        #label the face as smiling with text
+        #if there are coordinates for smiles
+        #i.e. list is not empty
+        #add text
+        if len(smiles) > 0:
+            cv2.putText(frame, 'smiling', (x, y+h+40), fontScale=3, fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255))
 
     #test if its working
     #will show coordinates of faces
